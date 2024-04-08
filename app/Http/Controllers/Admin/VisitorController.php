09687a93cd33d397f55c89671da7fb9feef52bc3
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-04-08 20:35:02 +0700

    install laravel authentication and config database

---
+++ app/Http/Controllers/Admin/VisitorController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Visitor;
+
+class VisitorController extends Controller
+{
+    // get visitor details
+    public function GetVisitorDetails() {
+        $ip_address = $_SERVER['REMOTE_ADDR'];
+        date_default_timezone_set("Asia/Jakarta");
+        $visit_time = date("h:i:sa");
+        $visit_date = date("d-m-Y");
+
+        $result = Visitor::insert([
+            'ip_address' => $ip_address,
+            'visit_time' => $visit_time,
+            'visit_date' => $visit_date
+        ]);
+
+        return $result;
+
+    } // end method
+}
