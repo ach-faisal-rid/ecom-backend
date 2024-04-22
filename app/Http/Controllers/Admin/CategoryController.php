2677090d04e63c6047ac92b9eb505a906563e8f8
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-04-22 08:41:56 +0700

    select category subcategory and make json array

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -4,13 +4,26 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\SubCategory;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function AllCategory() {
         $categories = Category::all();
-        return $categories;
+        $categoryDetailsArray = [];
+
+        foreach ($categories as $value) {
+            $subcategory = SubCategory::where('category_name',
+            $value['category_name'])->get();
+
+            $item = [
+                'category_name' => $value['category_name'],
+                'category_image' => $value['category_image'],
+                'subcategory_name' => $subcategory
+            ];
+            array_push($categoryDetailsArray, $item);
+        }
+        return $categoryDetailsArray;
     }
 }
- 
